2b7bb549730dbcc85260b5ce7d64ed1b81c8911b
scqvf <mariyagolub91@gmail.com>
2026-03-11 13:46:09 +0300

    вроде сделала бд

---
+++ public/api/submit_form.php
@@ -0,0 +1,24 @@
+<?php
+$host = 'localhost';
+$db   = 'diplombase';
+$user = 'db_knopka';
+$pass = 'GKSPwBDcl9bdXVFf';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['user_name'] ?? '';
+        $email = $_POST['user_email'] ?? '';
+        $service = $_POST['service_type'] ?? 'Не выбрано';
+        $message = $_POST['user_message'] ?? '';
+
+        $sql = "INSERT INTO service_requests (user_name, user_email, service_type, user_message) VALUES (?, ?, ?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$name, $email, $service, $message]);
+        echo "<h1>Заявка отправлена!</h1>";
+    }
+} catch (PDOException $e) {
+    die("Ошибка подключения: " . $e->getMessage());
+}
